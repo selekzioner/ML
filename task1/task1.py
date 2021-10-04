a3fca6035903e4c8b34817e8d678eaa95a766140
selekzioner <a.a.martushev@gmail.com>
2021-10-04 21:42:20 +0700

    started task 6

---
+++ task1/task1.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from scipy.spatial import distance
+from matplotlib.colors import ListedColormap
+import numpy as np
+
